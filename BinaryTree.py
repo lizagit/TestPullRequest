0a4996674ebb0fa6250554d0f04c52ae96497e9f
Oceane Andreis <ocan3794@colorado.edu>
2020-11-02 22:31:00 -0700

    Create BinaryTree.py
    No errors, just missing methods

---
+++ BinaryTree.py
@@ -0,0 +1,20 @@
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+    # Create a method to insert a node
+    
+    
+    #Create a method to traverse the tree
+    
+    
+    def PrintTree(self):
+        print(self.data)
+
+root = Node(10)
+
+root.PrintTree()
