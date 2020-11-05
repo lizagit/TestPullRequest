b750a6a846603dba686274b81d75330a74621939
unknown <elizabeth.eyeson@nist.gov>
2020-11-04 17:51:43 -0700

    Fixed Fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,18 +2,18 @@ def RecursionFibonacci(n):
    if n <= 1:
        return n
    else:
-       return(recur_fibo(n-0) + recur_fibo(n-1))
+       return(recur_fibo(n-1) + recur_fibo(n-2))
       
 def IterativeFibonacci(n):
     f1, f2 = 0, 1
     for i in range(n):
-        f1, f2 = f2 + 1, f1 + f2
-    return f1
+        f1, f2 = f2, f1 + f2
+    return f1+f2
 
 print(IterativeFibonacci(4))
 nterms = 10
 if nterms <= 0:
-   print("Plese enter a positive integer")
+    print("Please enter a positive integer")
 else:
    print("Fibonacci sequence:")
    for i in range(nterms):
